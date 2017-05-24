be4189d4d419a912eba8a0c2cf2a60bcaa52bebc
AQHawk <ironhawkcreations@gmail.com>
2017-05-23 23:21:17 -0700

    Add files via upload

---
+++ Hawk - Nifty Tours Template/index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+
+<!-- 
+Developer: Adrian Hawk
+-->
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Nifty Tours">
+    <meta name="description" content="Paris Tours with Nifty Travel">
+    <meta name="keywords" content="Nifty Travel, Paris Tour, European Tours, Tour Europe">
+    <title>Nifty Travel</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Raleway|Roboto" rel="stylesheet">
+  
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="css/generic.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/grid.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/meanmenu.css" media="all" />
+
+
+</head>
+
+<body>
+  <div class="topupperbar"></div>
+  <div class="toplowerbar"></div>
+  <div class="mainblock">
+    <div class="centerwrap">
+    
+     
+<?php include 'includes/header.inc.php'; ?>
+      
+  <div class="clearit"></div>
+      
+<div class="section group">
+  
+    <div class="col span_6_of_12">
+	<h1>Heading 1</h1>
+     <p>p font</p>
+	  <ul>
+	    <li>list item</li>
+	    <li>list item</li>
+	    <li>list item</li>
+	    <li>list item</li>
+	    <li>list item</li>
+	    <li>list item</li>
+      </ul>
+    </div>
+	
+    <div class="col span_6_of_12">
+    <h1>Slideshow</h1>
+         
+    </div>
+  </div>
+</div>
+</div>
+   
+  
+   
+  <div class="midbar"></div>
+   <div class="midtextblock"> 
+   <div class="centerwrap">
+    <div class="section group">
+      <div class="col span_4_of_12">
+	<h2>Heading 2</h2>
+     <p>Aliquam rutrum eget velit quis sagittis. Aliquam dignissim ligula lectus, at faucibus arcu venenatis sed. Aenean efficitur enim quis justo malesuada auctor. Nullam et accumsan sem. Integer finibus .</p>
+        <p>&nbsp;</p>
+    <h2>IMAGE HERE</h2>    
+    </div>
+      <div class="col span_4_of_12">
+	<h2>Heading 2</h2>
+     <p>Aliquam rutrum eget velit quis sagittis. Aliquam dignissim ligula lectus, at faucibus arcu venenatis sed. Aenean efficitur enim quis justo malesuada auctor. Nullam et accumsan sem. Integer finibus laoreet velit, non viverra nulla tincidunt id. Mauris semper mattis ligula, eu vulputate ligula fermentum nec. Duis ullamcorper erat ligula, bibendum suscipit urna molestie eget. Donec arcu purus, faucibus vitae vulputate sed, viverra at nunc. Aliquam dolor dui, venenatis quis iaculis sed, facilisis sed dui.</p>
+    </div>
+      <div class="col span_4_of_12">
+	<h2>Heading 2</h2>
+     <p>Aliquam rutrum eget velit quis sagittis. Aliquam dignissim ligula lectus, at faucibus arcu venenatis sed. Aenean efficitur enim quis justo malesuada auctor. Nullam et accumsan sem. Integer finibus laoreet velit, non viverra nulla tincidunt id. Mauris semper mattis ligula, eu vulputate ligula fermentum nec. Duis ullamcorper erat ligula, bibendum suscipit urna molestie eget. Donec arcu purus, faucibus vitae vulputate sed, viverra at nunc. Aliquam dolor dui, venenatis quis iaculis sed, facilisis sed dui.</p>
+    </div>
+      
+      
+      
+      
+      
+     </div>
+     
+     
+     
+  <div class="clearit"></div>
+    </div>
+    </div>  
+    
+    
+ <div class="footerbar">
+  <div class="centerwrap"> 
+  
+<?php include 'includes/footer.inc.php'; ?> 
+    
+    
+ <div class="clearit"></div>
+   </div>
+    </div>
+
+    
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+<script src="js/jquery.meanmenu.min.js"></script>
+<script>
+	jQuery(document).ready(function () {
+	    jQuery('header nav').meanmenu();
+	});
+</script>
+
+
+  
+    
+</body>
+
+</html>
