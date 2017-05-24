d8ddd0533d9fec6da610534988ed2c43a50dd6a4
AQHawk <ironhawkcreations@gmail.com>
2017-05-23 23:21:03 -0700

    Add files via upload

---
+++ Hawk - Nifty Tours Template/includes/footer.inc.php
@@ -0,0 +1,21 @@
+<footer>
+  
+  <div class="section group">
+  
+    <div class="col span_6_of_12">
+	 <div class="social">
+      <a href="http://www.facebook.com/NiftyTours" target="_blank"><img src="./images/facebook-logo.png" width="18" height="25" alt="Facebook link"><span>Facebook</span></a>
+      <a href="https://twitter.com/NiftyTours" target="_blank"><img src="./images/twitter-logo.png" width="34" height="25" alt="Twitter link"><span>Twitter</span></a>
+      </div>
+   
+    </div>
+	
+    <div class="col span_6_of_12">
+      <div class="copyright">
+    <p>Nifty Tours &copy; 2003-2017<br />
+          Website Designed by Jane Doe</p> 
+      </div>   
+    </div>
+  </div>
+  
+</footer>
