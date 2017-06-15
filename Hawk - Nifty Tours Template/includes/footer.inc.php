efcee7b5b75a0d827f7b456363c677bf212ec040
AQHawk <ironhawkcreations@gmail.com>
2017-06-15 13:43:25 -0700

    Add files via upload

--- Hawk - Nifty Tours Template/includes/footer.inc.php
+++ Hawk - Nifty Tours Template/includes/footer.inc.php
@@ -4,8 +4,8 @@
   
     <div class="col span_6_of_12">
 	 <div class="social">
-      <a href="http://www.facebook.com/NiftyTours" target="_blank"><img src="./images/facebook-logo.png" width="18" height="25" alt="Facebook link"><span>Facebook</span></a>
-      <a href="https://twitter.com/NiftyTours" target="_blank"><img src="./images/twitter-logo.png" width="34" height="25" alt="Twitter link"><span>Twitter</span></a>
+      <a href="http://www.facebook.com/cas222cascade" target="_blank"><img src="../images/facebook-logo.png" width="18" height="25" alt="Facebook link"><span>Facebook</span></a>
+      <a href="https://twitter.com/pcccas222" target="_blank"><img src="../images/twitter-logo.png" width="34" height="25" alt="Twitter link"><span>Twitter</span></a>
       </div>
    
     </div>
